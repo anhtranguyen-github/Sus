5976c9bf6662d60ab5399eab6a20a924bc320e11
anhtranguyen-github <tranguyen.contact@gmail.com>
2024-11-30 03:22:05 +0700

    add download_data.y

--- project3/main.py
+++ project3/main.py
@@ -3,6 +3,21 @@ import json
 import psycopg2
 import logging
 
+
+
+import gdown
+
+# Replace this with your folder's ID from the Google Drive link
+folder_id = '1FoENK70hR1xi4cx_HrKc1ASiZ9voXQYb'
+
+# Construct the Google Drive download URL for the folder
+download_url = f"https://drive.google.com/uc?id={folder_id}"
+
+# Download the folder
+gdown.download_folder(download_url, quiet=False)
+
+
+
 # Configure logging
 logging.basicConfig(
     level=logging.INFO,
@@ -29,7 +44,7 @@ except Exception as e:
     exit()
 
 # Directory containing JSON files
-json_dir = '../project2/tiki_product_data'
+json_dir = '../tiki_product_data'
 
 # Loop through each JSON file and insert data
 for filename in sorted(os.listdir(json_dir)):
